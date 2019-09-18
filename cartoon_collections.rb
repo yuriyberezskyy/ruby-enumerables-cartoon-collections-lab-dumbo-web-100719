8aef294ecdb75baa0e55bcde5315fad96a22d0f9
Yuriy Berezskyy <bereza95@gmail.com>
2019-09-18 03:03:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def  roll_call_dwarves(dwarves)
+    dwarves.each_with_index{ |person,index| 
+    puts "#{index+1}.*#{person}"
+    }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(arr)
+   arr.collect{
+     |element| element.capitalize
+   }
+   
+   arr.collect{
+     |element|  "#{element.capitalize}!"
+   }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  arr.include?("earth")
 end
 
 def find_the_cheese# code an argument here
